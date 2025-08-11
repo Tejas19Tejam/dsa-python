7df350f47f43798441742df59ebfc9c1567ec072
Vasudev <vasudevtejam@outlook.com>
2025-08-11 19:45:21 +0530

    Change folder

---
+++ code/patterns/patterns.py
@@ -0,0 +1,22 @@
+# Pattern 1
+
+# for i in range(0, 4):
+#     for j in range(0, 4):
+#         print("*", end="")
+#     print("")
+
+
+# Pattern 2
+# for i in range(0, 5):
+#     for j in range(0, i):
+#         print("* ", end="")
+#     print(" ")
+
+# Pattern 3
+# for i in range(0, 5):
+#     for j in range(0, 5 - i):
+#         print("* ", end="")
+#     print("")
+
+
+# Pattern 4
