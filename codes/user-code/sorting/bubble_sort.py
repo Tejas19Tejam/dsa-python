aea9707ca81d2c39d5f87527ef0b145c2b0316f0
Tejas19Tejam <vasudevtejam@outlook.com>
2025-11-13 21:43:57 +0530

    Bubble sort is done

--- codes/user-code/sorting/bubble_sort.py
+++ codes/user-code/sorting/bubble_sort.py
@@ -3,6 +3,7 @@
 ar = [12, 2, 1, 45, 30, 90]
 
 
+# Worst case Time Complexity : O(n^2)
 def bubble_sort(ar):
     for j in range(0, len(ar)):
         for i in range(0, len(ar) - j - 1):
@@ -13,4 +14,28 @@ def bubble_sort(ar):
     print(ar)
 
 
-bubble_sort(ar)
+# We can make a optimization lets  say you are given an array if already sorted
+
+
+ar1 = [1, 2, 3, 4, 5, 6]  # sorted array
+
+
+def optimized_bubble_sort(ar):
+    for i in range(0, len(ar)):
+        isSwapped = False
+        print("Done")
+        for j in range(0, (len(ar) - i) - 1):
+            if ar[j] > ar[j + 1]:
+                temp = ar[j + 1]
+                ar[j + 1] = ar[j]
+                ar[j] = temp
+                isSwapped = True
+        if not isSwapped:
+            print("O")
+            break
+
+
+# bubble_sort(ar)
+optimized_bubble_sort(ar1)
+
+# The time complexity of optimized one is O(N)
