33920ba59fb1aa614b0d8c1c6a0a12ec45991358
Tejas19Tejam <vasudevtejam@outlook.com>
2025-11-13 23:38:48 +0530

    Merge sort start

---
+++ codes/user-code/sorting/merge_sort.py
@@ -0,0 +1,6 @@
+# Insertion sort, selection sort and quick sort takes a O(n^2) time complexity to solve a problem
+# But merge sort is a much more optimized algorithm
+
+
+# Introduction about merge sort
+# The main concept of algorithm : DIVIDE AND MERGE
